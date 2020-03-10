7f171b4d9cc8008bfaea0006416d86299b8a8066
Momcilo Miladinovic <momcilo@basiq.io>
2020-03-10 10:39:39 +0100

    Added new project

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    ),
+  );
+}
