9839d9b87dfcbcc599f53954ea1ca394cac38a35
xtopala <xtopala@gmail.com>
2020-03-11 15:34:14 +0100

    Started xylophone

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Click me'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
