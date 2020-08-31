4050f4a4a65682fc43e257bbf3ea53f754063dc1
xtopala <xtopala@gmail.com>
2020-08-31 09:16:18 +0200

    Started chat project

---
+++ chat/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chat/screens/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
